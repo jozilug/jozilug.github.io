21c2d4ae69c29d95f2da058400135031da78b7ba
Daniel Fowler <dan.d.fowler@gmail.com>
2014-03-02 13:31:08 +0000

    Made some rubocop-related recommendations on meetup plugin.

--- _plugins/meetup.rb
+++ _plugins/meetup.rb
@@ -1,50 +1,47 @@
+# Encoding: utf-8
 require 'rmeetup'
 require 'markaby'
 
 module Jekyll
+  # This is where missing top-level class documentation comment goes
   class Meetup < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
-      
-      secrets = YAML::load(File.read('_secrets.yml'))
-      RMeetup::Client.api_key = secrets['meetup_api_key']
-      @text = text
+      secrets = YAML.load(File.read('_secrets.yml'))
+      meetup = RMeetup::Client
+      meetup.api_key = secrets['meetup_api_key']
+      @events = meetup.fetch(:events,
+                             group_urlname: 'Jozi-Linux-User-Group-JLUG')
+      @text = text.split(' ').first
     end
-    
-    def render(context)
 
-      events = RMeetup::Client.fetch(:events,{:group_urlname => "Jozi-Linux-User-Group-JLUG"})
+    def render(context)
       mab = Markaby::Builder.new
-      if @text == "latest "
-      then
-        unless events.empty?
-        then
-          event = events.first
-          mab.span :class => 'vevent' do
-            mab.a :href => event.event_url do 
-              
-              mab.span :class => 'summary' do
+      if @text == 'latest'
+        if @events.empty?
+          mab.a href: 'http://www.google.com' do
+            'No meetup scheduled yet'
+          end
+        else
+          event = @events.first
+          mab.span class: 'vevent' do
+            mab.a href: event.event_url do
+              mab.span class: 'summary' do
                 event.name
               end
               mab.span do
-                " on " + event.time.strftime("%d %B @ %H:%M").to_s
+                ' on ' + event.time.strftime('%d %B @ %H:%M').to_s
               end
             end
-            
-            mab.span :class => 'dtstart', :style => 'display: none;' do
+            mab.span class: 'dtstart', style: 'display: none;' do
               event.time.iso8601.to_s
             end
-          end        
-        else 
-          mab.a :href => 'http://www.google.com' do 
-            "No meetup scheduled yet"
           end
         end
       end
-      
       mab.to_s
     end
   end
 end
-  
+
 Liquid::Template.register_tag('meetup', Jekyll::Meetup)
