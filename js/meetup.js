844e9e9613e9c01d5313c0a37907171b86158a94
Daniel Fowler <dan.d.fowler@gmail.com>
2015-07-02 16:52:03 +0700

    Single var pattern, remove meetup dep. on jQuery, use ids

--- js/meetup.js
+++ js/meetup.js
@@ -1,39 +1,38 @@
-/*jslint indent: 2 */
+/*jshint browser: true */
 // Loads information about upcoming JoziLUG meetups via signed Meetup
 // API request loaded using JSONP method.  See:
 // http://en.wikipedia.org/wiki/JSONP
 
-"use strict";
-
 var fetchevents = function (data) {
+  'use strict';
+
   var events = data.results.filter(function (event) {
     return (event.id !== '168152892');
-  });
-  var last = function (limit,events) {
-    return events.splice(0, limit);
-  };
-  var link = function (event) {
-    return "<a href=\"" + event.event_url + "\">" + event.name + "</a>";
-  };
-  var write_to = function (target, content) {
-    target.empty().append(content);
-  };
-
-  // definitions
-  var upcoming_events = events.filter(function (event) {
-    return (event.status === 'upcoming');
-  });
-  var past_events = events.filter(function (event) {
-    return (event.status === 'past');
-  });
+  }),
+      last = function (limit, events) {
+        return events.splice(0, limit);
+      },
+      link = function (event) {
+        return '<a href=\'' + event.event_url + '\'>' + event.name + '</a>';
+      },
+      write_to = function (target, content) {
+        target.innerHTML = content;
+      },
+      upcoming_events = events.filter(function (event) {
+        return (event.status === 'upcoming');
+      }),
+      past_events = events.filter(function (event) {
+        return (event.status === 'past');
+      });
 
   // push recent events
   if (upcoming_events.length > 0) {
-    write_to($("p.nextup"), "Next up: " + link(upcoming_events.reverse()[0]));
+    write_to(document.getElementById('nextup'), 'Next up: ' + link(upcoming_events.reverse()[0]));
   } else {
-    write_to($("p.nextup"), "No upcoming events. :(");
+    write_to(document.getElementById('nextup'), 'No upcoming events. :(');
   }
-  write_to($("ul.recentmeetups"), last(10,past_events).map(function (event) {
-    return "<li>" + link(event) + "</li>";
-  }));
+
+  write_to(document.getElementById('recentmeetups'), last(10,past_events).map(function (event) {
+    return '<li>' + link(event) + '</li>';
+  }).join(''));
 };
